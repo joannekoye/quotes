8ec9f60e68935c84492a53525eaf3f6e853612aa
Joan Nekoye <joannekoye@gmail.com>
2020-03-31 16:03:55 +0300

    Edit date pipe

--- src/app/date-ago.pipe.ts
+++ src/app/date-ago.pipe.ts
@@ -5,8 +5,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateAgoPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): any{
+
+    let today:Date = new Date ()//get current date and time
+    let todayWithoutTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    var dateDifference = Math.abs(todayWithoutTime - value)//returns value in milliseconds
+    const secondsInDay = 86400
+    var dateDifferenceInSeconds = dateDifference*0.001
+    var dateCounter = dateDifferenceInSeconds/secondsInDay;
+    const alert = 'Invalid date'
+
+    if (dateCounter>=1 && todayWithoutTime > value){
+      return dateCounter;
+    } else{
+      return alert;
+    }
   }
+  
 
 }
