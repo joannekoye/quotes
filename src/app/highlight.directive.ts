e2ae2a606443e3cf1ad857380645672217893aad
Joan Nekoye <joannekoye@gmail.com>
2020-04-01 12:41:41 +0300

    EDit highlight directive

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,13 +1,19 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(private elem:ElementRef) {
-    this.elem.nativeElement.style.backgroundColor='lightcoral';
-    
+  constructor(private elem:ElementRef) {}
+
+  @HostListener("click") onClicks(index){
+   
+
   }
-  
+  private textDeco(action: string){
+  this.elem.nativeElement.style.backgroundColor= action;
+  }
+ 
 }
